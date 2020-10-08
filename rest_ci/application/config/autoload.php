25b479959b077ac3663bd8dc42cc7e6c73411973
Zainal Abidin <zainalabidin170197@gmail.com>
2020-10-08 21:18:19 +0700

    Saya hanya coba punya mas yoga

--- rest_ci/application/config/autoload.php
+++ rest_ci/application/config/autoload.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
-| AUTO-LOADER
+| AUTO-LOADER Saya di sini hanya coba coba
 | -------------------------------------------------------------------
 | This file specifies which systems should be loaded by default.
 |
